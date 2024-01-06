c2bc0667271c1ee0362723275ff4cdd7d50dfe15
serene1004 <vcbxnzn@naver.com>
2024-01-06 16:39:21 +0900

    visual 페이지 이미지모달 완료

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -1,8 +1,78 @@
 const style = {
-    visualAnimation : function() {
-        document.addEventListener("DOMContentLoaded", function() {
+    visualAnimation: function () {
+        document.addEventListener("DOMContentLoaded", function () {
             console.log('Start Animation');
-            
+
         });
     }
+}
+
+const utils = {
+    modal: {
+        init: function () {
+            const modalWrapper = document.querySelectorAll('.modal-wrapper');
+            const dim = document.querySelectorAll('.modal-wrapper .dim');
+            const closeBtn = document.querySelectorAll('.modal-wrapper .modal_close');
+        
+            function closeModal() {
+                modalWrapper.forEach(el => {
+                    el.classList.remove('on');
+                });
+            }
+        
+            dim.forEach(el => {
+                el.addEventListener('click', closeModal);
+            });
+        
+            closeBtn.forEach(el => {
+                el.addEventListener('click', closeModal);
+            });
+        },
+        open: function (id) {
+            const modalId = id || event.currentTarget.getAttribute('data-modal');
+
+            if (!modalId) {
+                return;
+            }
+
+            const modalWrapper = document.querySelector(`#${modalId}`);
+            modalWrapper.classList.add('on');
+        },
+        close: function (modalId) {
+            const modalWrapper = document.querySelector(`#${modalId}`);
+            modalWrapper.classList.remove('on');
+        }
+    },
+    imageModal: {
+        init: function (modalId) {
+            const modalWrapper = document.querySelector(`#${modalId}`);
+            const dim = document.querySelector(`#${modalId} .dim`);
+        
+            function closeModal() {
+                modalWrapper.classList.remove('on');
+                document.querySelector('html').removeAttribute('style', 'overflow');
+            }
+            
+            dim.addEventListener('click', closeModal);
+        },
+        open: function (id) {
+            const modalId = id || event.currentTarget.getAttribute('data-modal');
+            const src = event.target.getAttribute('src');
+
+            if (!modalId) {
+                return;
+            }
+
+            const modalWrapper = document.querySelector(`#${modalId}`);
+            modalWrapper.classList.add('on');
+            document.querySelector('html').style.overflow = 'hidden';
+
+            const image = modalWrapper.querySelector('img');
+            image.setAttribute('src', src);
+        },
+        close: function (modalId) {
+            const modalWrapper = document.querySelector(`#${modalId}`);
+            modalWrapper.classList.remove('on');
+        }
+    },
 }
